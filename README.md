f08d49e35ab6d22432ecd0ab37fb6e5e03c75333
Gil Mizrahi <gilmi@posteo.net>
2022-11-11 22:22:56 +0200

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# A minimal todo list app example with sqlite-easy
+
+This is a very simple example of the
+[sqlite-easy](https://hackage.haskell.org/package/sqlite-easy) package.
+
+- Read the [sqlite-easy docs](https://hackage.haskell.org/package/sqlite-easy-0.1.0.0/docs/Database-Sqlite-Easy.html)
+- See the example [source code](src/Main.hs)
+
+## How to build
+
+```sh
+cabal build
+```
+
+## What does it look like
+
+```sh
+>  ./todo-sqlite-easy
+Todo list app
+
+Set the connection string using the DB environment variable.
+
+Commands:
+
+	list		List all tasks
+	add <task>	Add a task
+	delete <id>	Delete a task by id
+	clear		Delete all tasks
+
+> ./todo-sqlite-easy add read the sqlite-easy docs
+New task with id (1)
+> ./todo-sqlite-easy add clone the sqlite-easy-example-todo repo
+New task with id (2)
+> ./todo-sqlite-easy add cabal build
+New task with id (3)
+> ./todo-sqlite-easy add play with the source code at src/Main.hs
+New task with id (4)
+> ./todo-sqlite-easy add "???"
+New task with id (5)
+> ./todo-sqlite-easy add "fun!"
+New task with id (6)
+> ./todo-sqlite-easy list
+6 tasks.
+	(1) read the sqlite-easy docs
+	(2) clone the sqlite-easy-example-todo repo
+	(3) cabal build
+	(4) play with the source code at src/Main.hs
+	(5) ???
+	(6) fun!
+> ./todo-sqlite-easy delete 5
+Task deleted.
+> ./todo-sqlite-easy list
+5 tasks.
+	(1) read the sqlite-easy docs
+	(2) clone the sqlite-easy-example-todo repo
+	(3) caal build
+	(4) play with the source code at src/Main.hs
+	(6) fun!
+```
---
+++ src/Main.hs
@@ -0,0 +1,167 @@
+-- | A primitive todo list app as an example of sqlite-easy.
+
+{-# language OverloadedStrings #-}
+{-# language LambdaCase #-}
+{-# language BlockArguments #-}
+
+module Main where
+
+import Database.Sqlite.Easy
+import Control.Monad (void)
+import Data.Functor ((<&>))
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import System.Environment (getArgs, lookupEnv)
+import Data.String (fromString)
+
+main :: IO ()
+main = do
+  connStr <- maybe "/tmp/temp_todo_sqlite-easy.db" fromString <$> lookupEnv "DB"
+  db <- mkDB connStr
+  input <- getArgs
+  act db input
+
+-- | Handle input and act accordingly
+act :: DB -> [String] -> IO ()
+act db = \case
+  ["list"] -> do
+    tasks <- getTasks db
+    putStrLn $ show (length tasks) <> " tasks."
+    getTasks db >>= mapM_ \(id', task) -> do
+      putStr $ "\t(" <> show id' <> ") "
+      Text.putStrLn task
+
+  "add" : text -> do
+    let task = Text.pack (unwords text)
+    id' <- insertTask db task
+    putStrLn $ "New task with id (" <> show id' <> ")"
+
+  ["delete", idStr] -> do
+    let id' = read idStr
+    exists <- deleteTaskById db id'
+    if exists
+      then putStrLn "Task deleted."
+      else errorMsg "A task by this id does not exist" ("(" <> show id' <> ")")
+
+  ["clear"] -> do
+    deleteTasks db
+    putStrLn "Tasks deleted."
+
+  _ ->
+    putStrLn $ unlines
+      [ "Todo list app"
+      , ""
+      , "Set the connection string using the DB environment variable."
+      , ""
+      , "Commands:"
+      , ""
+      , "\tlist\t\tList all tasks"
+      , "\tadd <task>\tAdd a task"
+      , "\tdelete <id>\tDelete a task by id"
+      , "\tclear\t\tDelete all tasks"
+      ]
+
+errorMsg :: String -> String -> a
+errorMsg msg extra =
+  error ("*** Error: " <> msg <> " - " <> extra)
+
+-----------------
+-- * Database
+
+-- ** Handler
+
+-- | Handler API type
+data DB
+  = DB
+    { getTasks :: IO [(Id, Task)]
+    , insertTask :: Task -> IO Id
+    , deleteTaskById :: Id -> IO Bool
+    , deleteTasks :: IO ()
+    }
+
+type Task = Text
+type Id = Int64
+
+-- | Handler smart constructor
+mkDB :: ConnectionString -> IO DB
+mkDB connectionString = do
+  pool <- createSqlitePool connectionString
+  withResource pool runMigrations
+  pure DB
+    { getTasks = getTasksFromDb pool
+    , insertTask = insertTaskToDb pool
+    , deleteTaskById = deleteTaskByIdFromDb pool
+    , deleteTasks = deleteTasksFromDb pool
+    }
+
+---------------------
+-- ** Migrations
+
+-- | Migrations action
+runMigrations :: Database -> IO ()
+runMigrations = migrate migrations migrateUp migrateDown
+
+-- | Migrations list
+migrations :: [MigrationName]
+migrations =
+  [ "task-table"
+  ]
+
+-- | Migrations up step
+migrateUp :: MigrationName -> Database -> IO ()
+migrateUp = \case
+  "task-table" ->
+    void . run "CREATE TABLE task(id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT)"
+  unknown ->
+    errorMsg "Unexpected migration" (show unknown)
+
+-- | Migrations down step
+migrateDown :: MigrationName -> Database -> IO ()
+migrateDown = \case
+  "task-table" ->
+    void . run "DROP TABLE task"
+  unknown ->
+    errorMsg "Unexpected migration" (show unknown)
+
+---------------------------
+-- ** Database actions
+
+-- | Retrieve all of the tasks from the database
+getTasksFromDb :: Pool Database -> IO [(Id, Task)]
+getTasksFromDb pool = do
+  withResource pool (run "SELECT id, task FROM task")
+    <&> map \case
+      [SQLInteger id', SQLText task] ->
+        (id', task)
+      result ->
+        errorMsg "Unexpected row" (show result)
+
+-- | Insert a new task into the database and get its id
+insertTaskToDb :: Pool Database -> Task -> IO Id
+insertTaskToDb pool task =
+  withResource pool \db ->
+    asTransaction db do
+      void $ runWith "INSERT INTO task(task) VALUES (?)" [SQLText task] db
+      result <- run "SELECT id FROM task ORDER BY id DESC LIMIT 1" db
+      case result of
+        [[SQLInteger lastId]] -> pure lastId
+        _ -> errorMsg "Unexpected row" (show result)
+
+-- | Delete a task by its id if it exists.
+--   Return whether there was a task by that id or not
+deleteTaskByIdFromDb :: Pool Database -> Id -> IO Bool
+deleteTaskByIdFromDb pool id' =
+  withResource pool \db ->
+    asTransaction db do
+      [[SQLInteger count]] <-
+        runWith "SELECT COUNT(*) FROM task WHERE id = ?" [SQLInteger id'] db
+      void $ runWith "DELETE FROM task WHERE id = ?" [SQLInteger id'] db
+      pure (count > 0)
+
+-- | Delete all tasks.
+deleteTasksFromDb :: Pool Database -> IO ()
+deleteTasksFromDb pool =
+  withResource pool \db ->
+    asTransaction db do
+      void $ run "DELETE FROM task" db
+      void $ run "UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='task'" db
